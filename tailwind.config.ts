9d86b4758cf6786f12ae5039ca455e4aae36a214
Raiky Silva <125393178+RaikySilvaDEV@users.noreply.github.com>
2026-08-28 10:54:25 -0300

    style: adicionar configuracao Tailwind CSS

---
+++ tailwind.config.ts
@@ -0,0 +1,2 @@
+import type { Config } from 'tailwindcss';
+export default { content:['./index.html','./src/**/*.{js,ts,jsx,tsx}'], theme:{extend:{borderRadius:{'2xl':'1rem'}}}, plugins:[] } satisfies Config;
